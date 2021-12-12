32e49b025d4cde8d878161bac225761c788c7c33
Ramakanth789 <95361310+Ramakanth789@users.noreply.github.com>
2021-12-12 13:31:48 +0530

    authentication

--- src/components/AuthenticationPage/AuthenticationPage.tsx
+++ src/components/AuthenticationPage/AuthenticationPage.tsx
@@ -4,18 +4,32 @@ import Logo from "../../logo.png";
 import { Field, Form } from 'react-final-form';
 import { useCallback } from 'react';
 
-const requiredValidator = (value:string) =>{
-  console.log("checking required",value);
+const requiredValidator = (value: string) => {
+  console.log("checking required", value);
   return value ? undefined : "Required";
-} 
+}
+
+//http://emailregex.com/
+const emailValidator = (value: string) => {
+  const result = value?.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
+  return result ? undefined : "Email is not proper";
+}
 //<TextField id="outlined-basic" label="User Name" variant="outlined" />
 //<TextField id="outlined-password-input" label="Password" type="password"  autoComplete="current-password"
 ///>
 export const BasicCard = () => {
 
   const onSubmit = useCallback((values) => {
- console.log(values);
-  }, [])
+    console.log(values);
+  }, []);
+
+  const emailInputRender = useCallback(({ input, meta }) => {
+    return <>
+      <input {...input}></input>
+      {meta.touched && meta?.error && <span>Email Not Valid</span>}
+    </>
+  }, []);
+
   return (
     <Card style={{ width: "100%" }}>
       <CardContent className="login_form">
@@ -24,10 +38,9 @@ export const BasicCard = () => {
 
           render={({ handleSubmit, invalid }) => (
             <form>
-              <Field name="email" component="input" validate={requiredValidator}></Field>
-              <Field name="password" component="input" validate={requiredValidator}></Field>
+              <Field name="email" validate={emailValidator} render={emailInputRender}/>
+              <Field name="password" validate={requiredValidator} component="input"></Field>
               <Button disabled={invalid} variant="contained" size="large" type="submit" onClick={handleSubmit}> Login </Button>
-
             </form>
           )}></Form>
 
